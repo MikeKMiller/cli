14a2f507df4a4d0ffa6174a4b0199457a0f87688
blacktop <blacktop@users.noreply.github.com>
2017-09-05 21:16:07 -0600

    fixes

--- cli/config/config.go
+++ cli/config/config.go
@@ -4,7 +4,7 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/maliceio/engine/cli/config/configfile"
+	"github.com/maliceio/cli/cli/config/configfile"
 	"github.com/maliceio/engine/pkg/homedir"
 	"github.com/pkg/errors"
 )
